9a85a09b96e43ec37b766752302c3bff1f3ada76
Renee Vandervelde <Renee@ReneeVandervelde.com>
2023-11-04 12:48:11 -0500

    Convert indicator to value class

--- app/src/main/java/com/inkapplications/glassconsole/ui/Indicators.kt
+++ app/src/main/java/com/inkapplications/glassconsole/ui/Indicators.kt
@@ -13,20 +13,20 @@ val Indicator.color
     @Composable
     @ReadOnlyComposable
     get() = when (this) {
-        Indicator.Nominal -> InkTheme.color.foreground
         Indicator.Primary -> InkTheme.color.accent
         Indicator.Positive -> InkTheme.color.success
         Indicator.Danger -> InkTheme.color.warning
         Indicator.Negative -> InkTheme.color.error
         Indicator.Idle -> InkTheme.color.dimForeground
+        else -> InkTheme.color.foreground
     }
 
 val Indicator.sound
     get() = when (this) {
-        Indicator.Nominal -> R.raw.nominal
         Indicator.Primary -> R.raw.primary
         Indicator.Positive -> R.raw.positive
         Indicator.Danger -> R.raw.danger
         Indicator.Negative -> R.raw.negative
         Indicator.Idle -> R.raw.idle
+        else -> R.raw.nominal
     }
--- structures/src/commonMain/kotlin/com/inkapplications/glassconsole/structures/Indicator.kt
+++ structures/src/commonMain/kotlin/com/inkapplications/glassconsole/structures/Indicator.kt
@@ -1,46 +1,43 @@
 package com.inkapplications.glassconsole.structures
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlin.jvm.JvmInline
 
 /**
  * Signifier that hints at the behavior of an element.
  */
+@JvmInline
 @Serializable
-enum class Indicator {
-    /**
-     * Use default signifiers
-     */
-    @SerialName("nominal")
-    Nominal,
+value class Indicator(val key: String) {
+    companion object {
+        /**
+         * Use default signifiers
+         */
+        val Nominal = Indicator("nominal")
 
-    /**
-     * Signify that this element is a main element amongst nominal emelents.
-     */
-    @SerialName("primary")
-    Primary,
+        /**
+         * Signify that this element is a main element amongst nominal emelents.
+         */
+        val Primary = Indicator("primary")
 
-    /**
-     * Signify that this element is in a successful or good state.
-     */
-    @SerialName("positive")
-    Positive,
+        /**
+         * Signify that this element is in a successful or good state.
+         */
+        val Positive = Indicator("positive")
 
-    /**
-     * Indicate a potential danger for an element.
-     */
-    @SerialName("danger")
-    Danger,
+        /**
+         * Indicate a potential danger for an element.
+         */
+        val Danger = Indicator("danger")
 
-    /**
-     * Indicate that an element is in a bad state.
-     */
-    @SerialName("negative")
-    Negative,
+        /**
+         * Indicate that an element is in a bad state.
+         */
+        val Negative = Indicator("negative")
 
-    /**
-     * Indicate that an item is in an inactive state.
-     */
-    @SerialName("idle")
-    Idle;
+        /**
+         * Indicate that an item is in an inactive state.
+         */
+        val Idle = Indicator("idle")
+    }
 }
