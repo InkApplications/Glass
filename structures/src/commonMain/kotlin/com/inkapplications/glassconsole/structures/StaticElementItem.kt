f5c5ad36691b04ae0c8f5a55525eb88b13b3c619
Renee Vandervelde <Renee@ReneeVandervelde.com>
2024-06-18 21:00:48 -0500

    Add extension function for static elements

--- structures/src/commonMain/kotlin/com/inkapplications/glassconsole/structures/StaticElementItem.kt
+++ structures/src/commonMain/kotlin/com/inkapplications/glassconsole/structures/StaticElementItem.kt
@@ -8,3 +8,12 @@ data class StaticElementItem(
     override val position: Positioning,
     override val span: Int,
 ): DisplayItem
+
+fun UiElement.Static.asDisplayItem(
+    position: Positioning = Positioning.Start,
+    span: Int = 1,
+) = StaticElementItem(
+    element = this,
+    position = position,
+    span = span,
+)
